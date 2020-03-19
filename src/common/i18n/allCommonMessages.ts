ed9d3632e05856f73567884de6f8e01b4f724465
Frode Hansen <frode.hansen@bekk.no>
2020-03-19 11:06:11 +0100

    Add common validation messages

--- src/common/i18n/allCommonMessages.ts
+++ src/common/i18n/allCommonMessages.ts
@@ -1,14 +1,18 @@
 const commonMessagesNb = require('common/i18n/common.nb.json');
 const commonMessagesNn = require('common/i18n/common.nn.json');
+const validationErrorsNb = require('common/i18n/validationErrors.nb.json');
+const validationErrorsNn = require('common/i18n/validationErrors.nn.json');
 const pictureScanningNb = require('common/components/picture-scanning-guide/picturescanningguide.nb.json');
 const pictureScanningNn = require('common/components/picture-scanning-guide/picturescanningguide.nn.json');
 
 const bokmålstekster = {
     ...commonMessagesNb,
+    ...validationErrorsNb,
     ...pictureScanningNb
 };
 const nynorsktekster = {
     ...commonMessagesNn,
+    ...validationErrorsNn,
     ...pictureScanningNn
 };
 
