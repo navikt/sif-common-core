14b59e3e452b56e475c59f17670fc4b62240a43f
Frode Hansen <frode.hansen@bekk.no>
2020-06-04 13:30:01 +0200

    feat(itemlist.tsx): replace trashcan with fjern text in itemlist
    Make default delete button in an item list be the text Fjern, not the trashcan

--- src/common/components/delete-button/DeleteButton.tsx
+++ src/common/components/delete-button/DeleteButton.tsx
@@ -2,25 +2,33 @@ import * as React from 'react';
 import bemHelper from '../../utils/bemUtils';
 import Trashcan from './TrashcanSvg';
 import './deleteButton.less';
+import ActionLink from '../action-link/ActionLink';
 
 interface DeleteButtonProps {
     ariaLabel: string;
-    onClick: (e: React.SyntheticEvent) => void;
+    useIcon?: boolean;
+    onClick: (e?: React.SyntheticEvent) => void;
 }
 
 const bem = bemHelper('deleteButton');
 
-const DeleteButton: React.FunctionComponent<DeleteButtonProps> = ({ ariaLabel, onClick }) => (
-    <button
-        type="button"
-        className={bem.block}
-        aria-label={ariaLabel}
-        onClick={(e) => {
-            e.stopPropagation();
-            onClick(e);
-        }}>
-        <Trashcan size={20} />
-    </button>
-);
+const DeleteButton: React.FunctionComponent<DeleteButtonProps> = ({ ariaLabel, useIcon = true, onClick }) => {
+    return useIcon ? (
+        <button
+            type="button"
+            className={bem.block}
+            aria-label={ariaLabel}
+            onClick={(e) => {
+                e.stopPropagation();
+                onClick(e);
+            }}>
+            <Trashcan size={20} />
+        </button>
+    ) : (
+        <ActionLink onClick={onClick} ariaLabel={ariaLabel}>
+            Fjern
+        </ActionLink>
+    );
+};
 
 export default DeleteButton;
--- src/dev/Intro.tsx
+++ src/dev/Intro.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import PageIntro from './components/page-intro/PageIntro';
+import ItemList from '../common/components/item-list/ItemList';
 
 interface Props {}
+
+interface Item {
+    name: string;
+    id: string;
+}
+
+const items: Item[] = [{ name: 'ABC', id: '1' }];
 
 const Intro: React.FunctionComponent<Props> = (props) => (
     <>
@@ -9,6 +17,12 @@ const Intro: React.FunctionComponent<Props> = (props) => (
             Dette er starten på en enkel guide til komponenter og konsepter i SIF-common, og som kan brukes i
             søknadsapplikasjonene til kapittel 9. Vi legger det vi trenger, etter hvert som vi ser at vi trenger det :)
         </PageIntro>
+        <ItemList<Item>
+            items={items}
+            getItemId={(item) => item.id}
+            getItemTitle={(item) => item.name}
+            onDelete={() => null}
+        />
     </>
 );
 
