f5723743f0e5a87e760d539f5337fc76d90cdd9f
Frode Hansen <frode.hansen@bekk.no>
2020-06-06 06:53:01 +0200

    feat(file): show info when file is to large

--- src/common/components/file-upload-errors/FileUploadErrors.tsx
+++ src/common/components/file-upload-errors/FileUploadErrors.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import { fileSizeIsValid } from 'common/utils/attachmentUtils';
+import prettyBytes from 'pretty-bytes';
 
-interface FileUploadErrorsProps {
+interface Props {
     filesThatDidntGetUploaded: File[];
 }
 
-const FileUploadErrors: React.FunctionComponent<FileUploadErrorsProps> = ({ filesThatDidntGetUploaded }) => {
+const FileUploadErrors: React.FunctionComponent<Props> = ({ filesThatDidntGetUploaded }: Props) => {
     if (filesThatDidntGetUploaded.length === 0) {
         return null;
     }
@@ -16,9 +18,19 @@ const FileUploadErrors: React.FunctionComponent<FileUploadErrorsProps> = ({ file
         <AlertStripeAdvarsel>
             <FormattedMessage id="fileUploadErrors.part1" />
             <ul>
-                {filesThatDidntGetUploaded.map(({ name }) => (
-                    <li key={name}>{name}</li>
-                ))}
+                {filesThatDidntGetUploaded.map(({ name, size, type }) => {
+                    return (
+                        <li key={name}>
+                            {name}
+                            {type}
+                            {size && !fileSizeIsValid(size) && (
+                                <div style={{ fontSize: '0.9rem' }}>
+                                    Fila er for stor ({prettyBytes(size)}). Maks filstørrelse er {prettyBytes(8000000)}.
+                                </div>
+                            )}
+                        </li>
+                    );
+                })}
             </ul>
         </AlertStripeAdvarsel>
     );
