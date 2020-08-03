f2d6d64a2d3d922249f198c38b8b8d83bd3fead7
Frode Hansen <frode.hansen@bekk.no>
2020-08-03 10:11:34 +0200

    feat(dev utils): add dev utils for intl
    Add utils for showing preview of intl messages in all supported languages

--- src/common/dev-utils/intl/messages-preview/MessagesPreview.tsx
+++ src/common/dev-utils/intl/messages-preview/MessagesPreview.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { AlertStripeFeil } from 'nav-frontend-alertstriper';
-import { Undertittel } from 'nav-frontend-typografi';
-import Box from '../../../common/components/box/Box';
-import bemUtils from '../../../common/utils/bemUtils';
-import {
-    createMultiLocaleObject, getMissingMessageKeys, MessageFileFormat
-} from '../../utils/devIntlUtils';
+import { AlertStripeFeil, AlertStripeInfo } from 'nav-frontend-alertstriper';
+import { Element, Ingress, Undertittel } from 'nav-frontend-typografi';
+import Box from '../../../components/box/Box';
+import bemUtils from '../../../utils/bemUtils';
+import { createMultiLocaleObject, getMissingMessageKeys, MessageFileFormat } from '../devIntlUtils';
 import './messagesPreview.less';
 
 interface Props {
@@ -14,7 +12,7 @@ interface Props {
 
 const bem = bemUtils('messagesList');
 
-const MessagesPreview: React.FunctionComponent<Props> = ({ messages }) => {
+const MessagesPreview = ({ messages }: Props) => {
     const allMessages = createMultiLocaleObject(messages);
     const missingMessages = getMissingMessageKeys(allMessages);
     return (
@@ -35,9 +33,38 @@ const MessagesPreview: React.FunctionComponent<Props> = ({ messages }) => {
                     </Box>
                 </>
             )}
-            <Box margin="xl">
-                <Undertittel>Alle tekstnøkler</Undertittel>
+            <Box margin="xl" padBottom="l">
+                <Undertittel>Søknad om pleiepenger utvidet rett</Undertittel>
             </Box>
+            <AlertStripeInfo>
+                <Ingress>Tegnforklaring</Ingress>
+                <p>
+                    Tekstene inneholder koder som brukes når applikasjonen setter inn verdier, og for å bestemme hvordan
+                    teksten skal se ut.
+                </p>
+                <p>
+                    <Element> Entall/flertall av en verdi</Element>
+                    <blockquote style={{ margin: 0, padding: '0.5rem 0' }}>
+                        <code>{`{timer, plural, one {# time} other {# timer}}`}</code>
+                    </blockquote>
+                    Kun ordene direkte etter # skal oversettes, resten er teknisk kode.
+                </p>
+                <p>
+                    <Element>Sett inn verdi i tekst</Element>
+                    <blockquote style={{ margin: 0, padding: '0.5rem 0' }}>
+                        <code>{`Første gyldige dato er {fom}, og siste gyldige dato er {tom}`}</code>
+                    </blockquote>
+                    Ord i klammer, f.eks. <code>{`{fom}`}</code>, blir erstattet med en verdi fra applikasjonen, og skal
+                    ikke oversettes.
+                </p>
+                <p>
+                    <Element>HTML-formatering</Element>
+                    <blockquote style={{ margin: 0, padding: '0.5rem 0' }}>
+                        <code>{`<p>En tekst som inneholder HTML kode</p>`}</code>
+                    </blockquote>
+                    All tekst, untatt tegn/ord i {`< >`} skal oversettes.
+                </p>
+            </AlertStripeInfo>
             <dl className={bem.block}>
                 {Object.keys(allMessages).map((key) => {
                     return (
