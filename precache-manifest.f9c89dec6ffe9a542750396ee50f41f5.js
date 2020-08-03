28befef1e81509667e7b702fabecaf50f53c6e9c
Frode Hansen <frode.hansen@bekk.no>
2020-08-03 13:12:16 +0200

    Updates

---
+++ precache-manifest.f9c89dec6ffe9a542750396ee50f41f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d4bcd3029fa67fb6c4e43be3781bd8",
+    "url": "/sif-common-core/index.html"
+  },
+  {
+    "revision": "2d0aa166503a28c952a9",
+    "url": "/sif-common-core/static/css/2.129a2bce.chunk.css"
+  },
+  {
+    "revision": "aa8a3169209860ffaf77",
+    "url": "/sif-common-core/static/css/main.523fdf5a.chunk.css"
+  },
+  {
+    "revision": "2d0aa166503a28c952a9",
+    "url": "/sif-common-core/static/js/2.bf72e989.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/sif-common-core/static/js/2.bf72e989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8a3169209860ffaf77",
+    "url": "/sif-common-core/static/js/main.a5bfd8e9.chunk.js"
+  },
+  {
+    "revision": "221dd71d334624f736af",
+    "url": "/sif-common-core/static/js/runtime-main.635cddca.js"
+  }
+]);
