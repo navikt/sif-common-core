cf195d8d9e3ccceabc8803489a288e331b777ebd
Andre Wolden <wolden.andre@gmail.com>
2020-08-12 15:11:52 +0200

    format code

--- src/common/components/knappelenke/Knappelenke.tsx
+++ src/common/components/knappelenke/Knappelenke.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import classnames from 'classnames';
 import './knappelenke.less';
 import Lenke from 'nav-frontend-lenker';
-import {Props as LenkeProps} from 'nav-frontend-lenker/lib/lenke';
+import { Props as LenkeProps } from 'nav-frontend-lenker/lib/lenke';
 
 export type KnappeLenkeType = 'hoved' | 'standard' | 'fare';
 
@@ -15,10 +15,11 @@ interface Props extends LenkeProps {
 
 const Knappelenke = ({ href, type, children, center, ...restProps }: Props) => {
     const knappeLenkeBody = (
-        <Lenke href={href} className={classnames('knapp', `knapp--${type || 'standard'}`, 'knappelenke')} {...restProps}>
+        <Lenke href={href}
+               className={classnames('knapp', `knapp--${type || 'standard'}`, 'knappelenke')} {...restProps}>
             <span>{children}</span>
         </Lenke>
-    )
+    );
     return center ? <div className={'knappelenke-wrapper'}>{knappeLenkeBody}</div> : knappeLenkeBody;
 };
 
