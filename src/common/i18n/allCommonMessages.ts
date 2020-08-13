0d94383272f2d93486e08cf6cf6ace630bd5327e
Frode Hansen <frode.hansen@bekk.no>
2020-08-13 10:31:07 +0200

    refactor(remove alias): remove alias common

--- src/common/i18n/allCommonMessages.ts
+++ src/common/i18n/allCommonMessages.ts
@@ -1,22 +1,22 @@
-const commonMessagesNb = require('common/i18n/common.nb.json');
-const commonMessagesNn = require('common/i18n/common.nn.json');
-const validationErrorsNb = require('common/i18n/validationErrors.nb.json');
-const validationErrorsNn = require('common/i18n/validationErrors.nn.json');
-const pictureScanningNb = require('common/components/picture-scanning-guide/picturescanningguide.nb.json');
-const pictureScanningNn = require('common/components/picture-scanning-guide/picturescanningguide.nn.json');
+const commonMessagesNb = require('./common.nb.json');
+const commonMessagesNn = require('./common.nn.json');
+const validationErrorsNb = require('./validationErrors.nb.json');
+const validationErrorsNn = require('./validationErrors.nn.json');
+const pictureScanningNb = require('../components/picture-scanning-guide/picturescanningguide.nb.json');
+const pictureScanningNn = require('../components/picture-scanning-guide/picturescanningguide.nn.json');
 
 const bokmålstekster = {
     ...commonMessagesNb,
     ...validationErrorsNb,
-    ...pictureScanningNb
+    ...pictureScanningNb,
 };
 const nynorsktekster = {
     ...commonMessagesNn,
     ...validationErrorsNn,
-    ...pictureScanningNn
+    ...pictureScanningNn,
 };
 
 export const allCommonMessages = {
     nb: bokmålstekster,
-    nn: nynorsktekster
+    nn: nynorsktekster,
 };
--- src/dev/components/app-intl-provider/AppIntlProvider.tsx
+++ src/dev/components/app-intl-provider/AppIntlProvider.tsx
@@ -4,7 +4,7 @@ import '@formatjs/intl-pluralrules/dist/locale-data/en';
 import '@formatjs/intl-pluralrules/dist/locale-data/nb';
 import '@formatjs/intl-pluralrules/dist/locale-data/nn';
 import '@formatjs/intl-pluralrules/polyfill';
-import { Locale } from 'common/types/Locale';
+import { Locale } from '../../../common/types/Locale';
 import { appMessages } from './messages';
 
 export interface IntlProviderProps {
