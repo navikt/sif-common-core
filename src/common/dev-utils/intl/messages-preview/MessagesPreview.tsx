fb96aeba991107458988d559c48f389abc219bd6
Frode Hansen <frode.hansen@bekk.no>
2020-08-17 09:54:08 +0200

    Split preview block to separate component

--- src/common/dev-utils/intl/messages-preview/MessagesPreview.tsx
+++ src/common/dev-utils/intl/messages-preview/MessagesPreview.tsx
@@ -6,6 +6,7 @@ import bemUtils from '../../../utils/bemUtils';
 import { createMultiLocaleObject, getMissingMessageKeys, MessageFileFormat } from '../devIntlUtils';
 import MessagesList from './MessagesList';
 import './messagesPreview.less';
+import MessagesPreviewExplanation from './MessagePreviewExplanation';
 
 interface Props {
     title?: string;
@@ -46,35 +47,9 @@ const MessagesPreview = ({
                 <Undertittel>{title}</Undertittel>
             </Box>
             {showExplanation && (
-                <AlertStripeInfo>
-                    <Ingress>Tegnforklaring</Ingress>
-                    <Box>
-                        Tekstene inneholder koder som brukes når applikasjonen setter inn verdier, og for å bestemme
-                        hvordan teksten skal se ut.
-                    </Box>
-                    <Box>
-                        <Element> Entall/flertall av en verdi</Element>
-                        <blockquote style={{ margin: 0, padding: '0.5rem 0' }}>
-                            <code>{`{timer, plural, one {# time} other {# timer}}`}</code>
-                        </blockquote>
-                        Kun ordene direkte etter # skal oversettes, resten er teknisk kode.
-                    </Box>
-                    <Box>
-                        <Element>Sett inn verdi i tekst</Element>
-                        <blockquote style={{ margin: 0, padding: '0.5rem 0' }}>
-                            <code>{`Første gyldige dato er {fom}, og siste gyldige dato er {tom}`}</code>
-                        </blockquote>
-                        Ord i klammer, f.eks. <code>{`{fom}`}</code>, blir erstattet med en verdi fra applikasjonen, og
-                        skal ikke oversettes.
-                    </Box>
-                    <Box>
-                        <Element>HTML-formatering</Element>
-                        <blockquote style={{ margin: 0, padding: '0.5rem 0' }}>
-                            <code>{`<Box>En tekst som inneholder HTML kode</Box>`}</code>
-                        </blockquote>
-                        All tekst, untatt tegn/ord i {`< >`} skal oversettes.
-                    </Box>
-                </AlertStripeInfo>
+                <Box>
+                    <MessagesPreviewExplanation />
+                </Box>
             )}
             <MessagesList messages={messages} />
         </div>
