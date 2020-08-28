9105de259ae3797ab64535d75a2ffd1069124949
Frode Hansen <frode.hansen@bekk.no>
2020-08-28 09:43:32 +0200

    feat(apiutils): add helper function
    Add helper function to check api errors if user is unauthorized or is forbidden

--- src/common/utils/apiUtils.ts
+++ src/common/utils/apiUtils.ts
@@ -7,6 +7,8 @@ export const isForbidden = (error: AxiosError): boolean =>
 export const isUnauthorized = (error: AxiosError): boolean =>
     error !== undefined && error.response !== undefined && error.response.status === HttpStatus.UNAUTHORIZED;
 
+export const isUserLoggedOut = (error: AxiosError): boolean => isForbidden(error) || isUnauthorized(error);
+
 export const getStartedSøknadRequestParam = (date?: Date): string | undefined => {
     return date ? `startetSøknad=${date.toISOString()}` : undefined;
 };
