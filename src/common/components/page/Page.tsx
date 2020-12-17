f4b06552663d0785ac527218b6b0e6bc2d0de480
Frode Hansen <frode.hansen@bekk.no>
2020-12-17 10:48:58 +0100

    Fix Page component

--- src/common/components/page/Page.tsx
+++ src/common/components/page/Page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useIntl } from 'react-intl';
 import intlHelper from '../../utils/intlUtils';
 
@@ -13,38 +13,35 @@ interface PageProps {
     setMainRole?: boolean;
     ariaLabel?: string;
     topContentRenderer?: () => React.ReactElement<any>;
+    children: React.ReactNode;
 }
 
-class Page extends React.Component<PageProps> {
-    componentDidMount() {
+const Page = ({
+    id = 'pageMainContent',
+    className,
+    title,
+    setMainRole = true,
+    ariaLabel,
+    topContentRenderer,
+    children,
+}: PageProps) => {
+    useEffect(() => {
         window.scrollTo(0, 0);
-    }
+    }, []);
 
-    render() {
-        const intl = useIntl();
-        const {
-            id = 'pageMainContent',
-            className,
-            title,
-            setMainRole = true,
-            ariaLabel,
-            topContentRenderer,
-            children,
-        } = this.props;
+    const intl = useIntl();
+    const role = setMainRole === true || ariaLabel !== undefined ? 'main' : undefined;
+    const ariaLabelToUse =
+        ariaLabel || setMainRole === true ? intlHelper(intl, 'page.defaultMainRoleLabel') : undefined;
 
-        const role = setMainRole === true || ariaLabel !== undefined ? 'main' : undefined;
-        const ariaLabelToUse =
-            ariaLabel || setMainRole === true ? intlHelper(intl, 'page.defaultMainRoleLabel') : undefined;
-
-        return (
-            <DocumentTitle title={title}>
-                <div role={role} aria-label={ariaLabelToUse} id={id}>
-                    {topContentRenderer && topContentRenderer()}
-                    <div className={`page ${className}`}>{children}</div>
-                </div>
-            </DocumentTitle>
-        );
-    }
-}
+    return (
+        <DocumentTitle title={title}>
+            <div role={role} aria-label={ariaLabelToUse} id={id}>
+                {topContentRenderer && topContentRenderer()}
+                <div className={`page ${className}`}>{children}</div>
+            </div>
+        </DocumentTitle>
+    );
+};
 
 export default Page;
