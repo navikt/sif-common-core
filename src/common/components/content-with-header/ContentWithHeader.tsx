1813f0f21c90e894f75f57ddcfbfbd5380ed144d
Frode Hansen <frode.hansen@bekk.no>
2021-03-16 13:06:41 +0100

    Legg til headerTag prop

--- src/common/components/content-with-header/ContentWithHeader.tsx
+++ src/common/components/content-with-header/ContentWithHeader.tsx
@@ -5,15 +5,18 @@ import './contentWithHeader.less';
 
 interface Props {
     header: string;
+    headerTag?: 'h1' | 'h2' | 'h3' | 'h4' | string;
     children: React.ReactNode;
 }
 
 const bem = bemHelper('contentWithHeader');
 
-const ContentWithHeader = ({ header, children }: Props) => {
+const ContentWithHeader = ({ header, headerTag, children }: Props) => {
     return (
         <div className={bem.block}>
-            <Normaltekst className={bem.element('header')}>{header}</Normaltekst>
+            <Normaltekst tag={headerTag} className={bem.element('header')}>
+                {header}
+            </Normaltekst>
             {children}
         </div>
     );
