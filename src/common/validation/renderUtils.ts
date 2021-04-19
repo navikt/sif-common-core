7144c5aff79857ff440881c1588a54b1c454c2d5
Frode Hansen <frode.hansen@bekk.no>
2021-04-19 11:36:31 +0200

    Legge til formName på error renderUtils

--- src/common/validation/renderUtils.ts
+++ src/common/validation/renderUtils.ts
@@ -2,17 +2,20 @@ import { IntlShape } from 'react-intl';
 import { FeiloppsummeringFeil } from 'nav-frontend-skjema';
 import intlHelper from '../utils/intlUtils';
 
-export const constructIntlValidationErrorKey = (error: string, fieldName: string): string =>
-    `validation.${fieldName}.${error}`;
+export const constructIntlValidationErrorKey = (error: string, fieldName: string, formName?: string): string =>
+    `${formName ? `${formName}.` : ''}validation.${fieldName}.${error}`;
 
-export const getFieldErrorRenderer = (intl: IntlShape) => (error: string, fieldName: string) => {
-    return intlHelper(intl, constructIntlValidationErrorKey(error, fieldName));
+export const getFieldErrorRenderer = (intl: IntlShape, formName?: string) => (error: string, fieldName: string) => {
+    return intlHelper(intl, constructIntlValidationErrorKey(error, fieldName, formName));
 };
 
-export const getSummaryFieldErrorRenderer = (intl: IntlShape) => (error: string, fieldName: string) => {
+export const getSummaryFieldErrorRenderer = (intl: IntlShape, formName?: string) => (
+    error: string,
+    fieldName: string
+) => {
     const feil: FeiloppsummeringFeil = {
         skjemaelementId: fieldName,
-        feilmelding: intlHelper(intl, constructIntlValidationErrorKey(error, fieldName)),
+        feilmelding: intlHelper(intl, constructIntlValidationErrorKey(error, fieldName, formName)),
     };
     return feil;
 };
