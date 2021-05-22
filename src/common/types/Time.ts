fd7c56d3a9a93ed77f2d78a29e806d1c81a2382a
Frode Hansen <frode.hansen@bekk.no>
2021-05-22 14:38:37 +0200

    Time som string og number

--- src/common/types/Time.ts
+++ src/common/types/Time.ts
@@ -1,4 +1,4 @@
 export interface Time {
-    hours: number;
-    minutes: number;
+    hours: number | string;
+    minutes: number | string;
 }
--- src/common/utils/__tests__/timeUtils.test.ts
+++ src/common/utils/__tests__/timeUtils.test.ts
@@ -4,11 +4,11 @@ describe('timeUtils', () => {
     it('should convert decimal hours to Time', () => {
         expect(decimalTimeToTime(1.5)).toEqual({
             hours: 1,
-            minutes: 30
+            minutes: 30,
         });
         expect(decimalTimeToTime(1.75)).toEqual({
             hours: 1,
-            minutes: 45
+            minutes: 45,
         });
     });
 
@@ -17,8 +17,18 @@ describe('timeUtils', () => {
         expect(timeToDecimalTime({ hours: 0, minutes: 30 })).toBe(0.5);
     });
 
+    it('should convert time with strings to decimal time', () => {
+        expect(timeToDecimalTime({ hours: '37', minutes: '30' })).toBe(37.5);
+        expect(timeToDecimalTime({ hours: '0', minutes: '30' })).toBe(0.5);
+    });
+
     it('should format time in Iso8601Duration format', () => {
         expect(timeToIso8601Duration({ hours: 10, minutes: 0 })).toEqual('PT10H0M');
         expect(timeToIso8601Duration({ hours: 0, minutes: 45 })).toEqual('PT0H45M');
+    });
+
+    it('should format time with strings in Iso8601Duration format', () => {
+        expect(timeToIso8601Duration({ hours: '10', minutes: '0' })).toEqual('PT10H0M');
+        expect(timeToIso8601Duration({ hours: '0', minutes: '45' })).toEqual('PT0H45M');
     });
 });
