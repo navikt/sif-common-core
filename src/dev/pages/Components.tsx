b8045976996f843c4ae1002efd15325bcbc89bb6
Frode Hansen <frode.hansen@bekk.no>
2021-08-25 13:02:00 +0200

    Small screen adjustment (nav-overrides)

--- src/dev/pages/Components.tsx
+++ src/dev/pages/Components.tsx
@@ -1,6 +1,8 @@
+import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 import React from 'react';
 import Box from '../../common/components/box/Box';
 import ContentWithHeader from '../../common/components/content-with-header/ContentWithHeader';
+import CounsellorPanel from '../../common/components/counsellor-panel/CounsellorPanel';
 import Knappelenke from '../../common/components/knappelenke/Knappelenke';
 import Page from '../../common/components/page/Page';
 import StepBanner from '../../common/components/step-banner/StepBanner';
@@ -13,6 +15,12 @@ const ComponentsView = () => {
                     <StepBanner text={'Sykdom i familien - SIF-Common'} />
                 </Box>
             </ContentWithHeader>
+            <AlertStripeFeil title="ABC">
+                Innhold som skal gå over flere linjer og som jaja, kan inneholdelangeordsomerutfordrende
+            </AlertStripeFeil>
+
+            <CounsellorPanel switchToPlakatOnSmallScreenSize={true}>Whoa Wha</CounsellorPanel>
+
             <ContentWithHeader header={'En lenke som ser ut som en Knapp'}>
                 <Box margin={'l'} padBottom={'xl'}>
                     <Knappelenke href={'#'} type={'hoved'}>
