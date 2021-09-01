270f12a3261b5329e4b5b3b639a496e6f25ed900
Frode Hansen <frode.hansen@bekk.no>
2021-09-01 10:43:58 +0200

    9.4.10

--- src/dev/Intro.tsx
+++ src/dev/Intro.tsx
@@ -3,7 +3,7 @@ import PageIntro from './components/page-intro/PageIntro';
 import ItemList from '../common/components/item-list/ItemList';
 import CounsellorPanel from '../common/components/counsellor-panel/CounsellorPanel';
 import FrontPageBanner from '../common/components/front-page-banner/FrontPageBanner';
-
+import Kvittering from '../common/components/kvittering/Kvittering';
 interface Item {
     name: string;
     id: string;
@@ -25,6 +25,19 @@ const Intro = () => (
             Dette er starten på en enkel guide til komponenter og konsepter i SIF-common, og som kan brukes i
             søknadsapplikasjonene til kapittel 9. Vi legger det vi trenger, etter hvert som vi ser at vi trenger det :)
         </PageIntro>
+
+        <Kvittering
+            tittel="Hey"
+            liste={{
+                tittel: 'Whoa',
+                punkter: [
+                    <>
+                        Dette er starten på en enkel guide til komponenter og konsepter i SIF-common, og som kan brukes
+                        i søknadsapplikasjonene til kapittel 9. Vi legger det vi trenger, etter hvert som vi ser at vi
+                        trenger det :)
+                    </>,
+                ],
+            }}></Kvittering>
         <ItemList<Item>
             items={items}
             getItemId={(item) => item.id}
