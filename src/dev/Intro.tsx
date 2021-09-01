d9d5910d9684c170bc36faa14e26f92a1b55e4aa
Frode Hansen <frode.hansen@bekk.no>
2021-09-01 10:14:51 +0200

    Layout - frontpageBanner

--- src/dev/Intro.tsx
+++ src/dev/Intro.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PageIntro from './components/page-intro/PageIntro';
 import ItemList from '../common/components/item-list/ItemList';
 import CounsellorPanel from '../common/components/counsellor-panel/CounsellorPanel';
+import FrontPageBanner from '../common/components/front-page-banner/FrontPageBanner';
 
 interface Item {
     name: string;
@@ -12,6 +13,14 @@ const items: Item[] = [{ name: 'ABC', id: '1' }];
 
 const Intro = () => (
     <>
+        <FrontPageBanner
+            bannerSize="large"
+            counsellorWithSpeechBubbleProps={{
+                strongText: 'Hei',
+                normalText: 'Jeg vil hjelpe deg med å fylle ut meldingen, slik at du kan dele omsorgsdager',
+            }}
+        />
+
         <PageIntro title="SIF-common guide">
             Dette er starten på en enkel guide til komponenter og konsepter i SIF-common, og som kan brukes i
             søknadsapplikasjonene til kapittel 9. Vi legger det vi trenger, etter hvert som vi ser at vi trenger det :)
