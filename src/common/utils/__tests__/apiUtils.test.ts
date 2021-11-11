167feb98ea89fe9bcdb605fceb679b2bd5e74f87
Frode Hansen <frode.hansen@bekk.no>
2021-11-11 08:41:48 +0100

    Legge til apiUtils test

---
+++ src/common/utils/__tests__/apiUtils.test.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { AxiosError } from 'axios';
+import { isForbidden, isUnauthorized } from '../apiUtils';
+
+let axiosErrorMock: AxiosError;
+
+const mockedApiUrl = 'mockedApiUrl';
+jest.mock('./../envUtils.ts', () => {
+    return { getEnvironmentVariable: () => mockedApiUrl };
+});
+
+describe('apiUtils', () => {
+    beforeEach(() => {
+        axiosErrorMock = {
+            config: {},
+            isAxiosError: false,
+            name: '',
+            message: '',
+            toJSON: () => ({}),
+            response: { status: 200, data: {}, statusText: '', headers: {}, config: {} },
+        };
+    });
+
+    describe('isForbidden', () => {
+        it('should return true if response.status is 403', () => {
+            axiosErrorMock.response!.status = 403;
+            expect(isForbidden(axiosErrorMock)).toBe(true);
+        });
+
+        it('should return false if response status is not 403', () => {
+            axiosErrorMock.response!.status = 200;
+            expect(isForbidden(axiosErrorMock)).toBe(false);
+            axiosErrorMock.response = undefined;
+            expect(isForbidden(axiosErrorMock)).toBe(false);
+        });
+    });
+
+    describe('isUnauthorized', () => {
+        it('should return true if response.status is 401', () => {
+            axiosErrorMock.response!.status = 401;
+            expect(isUnauthorized(axiosErrorMock)).toBe(true);
+        });
+
+        it('should return false if response status is not 401', () => {
+            axiosErrorMock.response!.status = 200;
+            expect(isUnauthorized(axiosErrorMock)).toBe(false);
+            axiosErrorMock.response = undefined;
+            expect(isUnauthorized(axiosErrorMock)).toBe(false);
+        });
+    });
+});
