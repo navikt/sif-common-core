6f7c830ec9f43d0f06b0a2b563a9676b9a2f1e2f
Frode Hansen <frode.hansen@bekk.no>
2021-11-11 07:43:02 +0100

    Håndtere 451 http error code

--- src/common/utils/apiUtils.ts
+++ src/common/utils/apiUtils.ts
@@ -1,13 +1,14 @@
 import { AxiosError } from 'axios';
-import HttpStatus from 'http-status-codes';
+import HttpStatus, { StatusCodes } from 'http-status-codes';
 
-export const isForbidden = (error: AxiosError): boolean =>
-    error !== undefined && error.response !== undefined && error.response.status === HttpStatus.FORBIDDEN;
+export const isForbidden = ({ response }: AxiosError) =>
+    response !== undefined &&
+    (response.status === HttpStatus.FORBIDDEN || response.status === StatusCodes.UNAVAILABLE_FOR_LEGAL_REASONS);
 
 export const isUnauthorized = (error: AxiosError): boolean =>
     error !== undefined && error.response !== undefined && error.response.status === HttpStatus.UNAUTHORIZED;
 
-export const isUserLoggedOut = (error: AxiosError): boolean => isForbidden(error) || isUnauthorized(error);
+export const isUserLoggedOut = (error: AxiosError): boolean => isUnauthorized(error);
 
 export const getStartedSøknadRequestParam = (date?: Date): string | undefined => {
     return date ? `startetSøknad=${date.toISOString()}` : undefined;
