dff877b7eaf423d9009536ae17399479016f4ce6
Frode Hansen <frode.hansen@bekk.no>
2022-02-06 11:13:03 +0100

    Legge til id-attributt på Box og FormBlock

--- src/common/components/box/Box.tsx
+++ src/common/components/box/Box.tsx
@@ -5,6 +5,7 @@ import './box.less';
 export type BoxMargin = 's' | 'm' | 'l' | 'xl' | 'xxl' | 'xxxl' | 'none';
 
 interface Props {
+    id?: string;
     margin?: BoxMargin;
     padBottom?: BoxMargin;
     textAlignCenter?: boolean;
@@ -14,7 +15,7 @@ interface Props {
 
 const bem = bemHelper('box');
 
-const Box = ({ margin, padBottom, className, textAlignCenter, children }: Props) => {
+const Box = ({ margin, padBottom, className, textAlignCenter, id, children }: Props) => {
     const classNames = bem.classNames(
         bem.block,
         bem.modifierConditional(margin, margin !== undefined),
@@ -24,7 +25,11 @@ const Box = ({ margin, padBottom, className, textAlignCenter, children }: Props)
             [`${className}`]: className !== undefined,
         }
     );
-    return <div className={classNames}>{children}</div>;
+    return (
+        <div className={classNames} id={id}>
+            {children}
+        </div>
+    );
 };
 
 export default Box;
--- src/common/components/form-block/FormBlock.tsx
+++ src/common/components/form-block/FormBlock.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Box, { BoxMargin } from '../box/Box';
 
 interface Props {
+    id?: string;
     margin?: BoxMargin;
     paddingBottom?: BoxMargin;
     children: React.ReactNode;
 }
 
-const FormBlock = ({ margin = 'xl', paddingBottom, children }: Props) => (
-    <Box margin={margin} padBottom={paddingBottom}>
+const FormBlock = ({ margin = 'xl', paddingBottom, children, id }: Props) => (
+    <Box margin={margin} padBottom={paddingBottom} id={id}>
         {children}
     </Box>
 );
