c73d581659dfeb4fafc2056f1d283d5d59f9fec8
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-11-02 08:21:40 +0100

    pkgUpd

--- src/common/components/box/__tests__/Box.test.tsx
+++ src/common/components/box/__tests__/Box.test.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
 import Box from '../Box';
 
 describe('<Box />', () => {
-    it('renders a <div> with correct classname specified from props', () => {
-        let box = shallow(<Box margin="s">s</Box>);
-        expect(box.find('div.box').hasClass('box--s')).toBe(true);
-        box = shallow(<Box margin="m">m</Box>);
-        expect(box.find('div.box').hasClass('box--m')).toBe(true);
-        box = shallow(<Box margin="l">l</Box>);
-        expect(box.find('div.box').hasClass('box--l')).toBe(true);
+    test('renders a <div> with correct classname specified from props', () => {
+        // let box = shallow(<Box margin="s">s</Box>);
+        render(<Box margin="s">s</Box>);
+        expect(screen.getByText('s')).toHaveClass('box--s');
+
+        render(<Box margin="m">m</Box>);
+        expect(screen.getByText('m')).toHaveClass('box--m');
+
+        render(<Box margin="l">l</Box>);
+        expect(screen.getByText('l')).toHaveClass('box--l');
     });
 
     it('renders children in a <div>', () => {
         const children = <span>Children</span>;
-        const box = shallow(<Box margin="s">{children}</Box>);
-        expect(box.find('div.box').contains(children)).toBe(true);
+        const { container } = render(<Box margin="s">{children}</Box>);
+        expect(container.firstChild).toHaveTextContent('Children');
     });
 });
